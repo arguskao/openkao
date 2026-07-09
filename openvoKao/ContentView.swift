2d1e83ba38556c0f4128c452ae161c7b650f7a45
Kao <arguskao@gmail.com>
2026-07-09 15:06:17 +0800

    Initial Commit

---
+++ openvoKao/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  openvoKao
+//
+//  Created by user on 2026/7/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ openvoKao/openvoKaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  openvoKaoApp.swift
+//  openvoKao
+//
+//  Created by user on 2026/7/9.
+//
+
+import SwiftUI
+
+@main
+struct openvoKaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
